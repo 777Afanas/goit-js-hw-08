d1e4a69c832989282a95ef49c6c15f4d4dc009bd
Serhii Yatsenko <serhii2111@yahoo.com>
2022-11-08 02:20:31 +0200

    task two

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,60 +6,34 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 
-const galleryContainer = document.querySelector('.gallery'); 
-
+const galleryContainer = document.querySelector('.gallery');
 const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
 
 galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
 
-galleryContainer.addEventListener('click', onGalleryContainerClick);
+// galleryContainer.addEventListener('click', onGalleryContainerClick);
 
 function createGalleryItemsMarkup(galleryItems) {
     return galleryItems
         .map(({ preview, original, description }) => {
         return `
-     <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
+    <a class="gallery__item" href="${original}">
     <img
       class="gallery__image"
       src="${preview}" 
-      data-source="${original}"
       alt="${description}"
     />
-  </a>
-</div>`;
-    }).join('');     
-} 
+  </a>`;
+    }).join('');
+}
 
-function onGalleryContainerClick(evt) {
-    evt.preventDefault();
-
-    if (evt.target.nodeName !== 'IMG') {
-        return;
-    };
-
-    const instance = basicLightbox.create
-        (`<img src="${evt.target.dataset.source}" width="800" height="600">`,
-            {
-                onShow: () => {
-                    window.addEventListener('keydown', onEscKeyPress);
-                },
-                onClose: () => {
-                    window.removeEventListener('keydown', onEscKeyPress);
-                },
-            });      
-         
-    instance.show();
-    console.log(evt.target.dataset.source);
-} 
-
-function onEscKeyPress(evt) {
-        if (evt.code === 'Escape') {
-            return;
-       }
-       instance.close();
-    }
-
-
+let gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionDelay: 250,
+  captionSelector: "img",
+  captionsData: "alt",
+  captionPosition: "bottom",
+});
 
 console.log(galleryItems);
+
