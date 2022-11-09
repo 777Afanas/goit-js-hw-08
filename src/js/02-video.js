a9c8efc264c3dd07ff418d808bc2a45e4911e184
Serhii Yatsenko <serhii2111@yahoo.com>
2022-11-09 11:49:13 +0200

    did everything

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,76 +4,30 @@
 // продолжать воспроизводить видео с этого времени.
 
 // Инициализируй плеер в файле скрипта как это описано в секции pre-existing player, 
-// но учти что у тебя плеер добавлен как npm пакет, а не через CDN.
+// но учти что у тебя плеер добавлен как npm пакет, а не через CDN.  
+
+// начни отслеживать событие timeupdate - обновление времени воспроизведения.
+
+
     import Player from '@vimeo/player';
     import throttle from 'lodash.throttle';                    
     
+
     const iframe = document.querySelector('iframe');
     const player = new Player(iframe);
-
-
-player.on('timeupdate', throttle (() => {
-    localStorage.setItem('videoplayer-current-time', seconds);
-  }, 1000)
-    );
-
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-    .catch(function (error) {
-        console.error(error)
-    });    
-
-
-// player.on('timeupdate', throttle (() => {
-//     localStorage.setItem('videoplayer-current-time', seconds);
-//   }, 1000)
-//     );
-
-// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-//     .catch(function (error) {
-//         console.error(error)
-//     }); 
-
-
-
-
-
-// Разбери документацию метода on() и 
-// начни отслеживать событие timeupdate - обновление времени воспроизведения.
-
-// const onPlay = function(data) {
-//     // data is an object containing properties specific to that event
-// };
-
-// player.on('play', onPlay);
+    LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
 // Сохраняй время воспроизведения в локальное хранилище. 
-// Пусть ключом для хранилища будет строка "videoplayer-current-time".
-
-//  localStorage. setItem('videoplayer-current-time', 'Значение');
+// Пусть ключом для хранилища будет строка "videoplayer-current-time". 
+player.on('timeupdate', throttle(function (data) {
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data.seconds));
+}), 1000);
 
 // При перезагрузке страницы воспользуйся методом setCurrentTime() 
 // для того чтобы возобновить воспроизведение с сохраненной позиции.
-
-// player.setCurrentTime(30.456).then(function(seconds) {
-//     // seconds = the actual time that the player seeked to
-// }).catch(function(error) {
-//     switch (error.name) {
-//         case 'RangeError':
-//             // the time was less than 0 or greater than the video’s duration
-//             break;
-
-//         default:
-//             // some other error occurred
-//             break;
-//     }
-// });
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
+    .catch(function (error) {
+        console.error(error)
+    }); 
 
 
-// Добавь в проект бибилотеку lodash.throttle и сделай так,
-// чтобы время воспроизведения обновлялось в хранилище не чаще чем раз в секунду.
-// document.addEventListener(
-//   "scroll",
-//   _.throttle(() => {
-//     console.log("Scroll handler call every 300ms");
-//   }, 300)
-// );
